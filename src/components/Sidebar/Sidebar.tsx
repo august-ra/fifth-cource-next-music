253daafc47cbd4ccf714fdaaed4e4b6e7e50c892
Anton Voytov <august-ra@ya.ru>
2024-09-08 18:49:21 +0300

    Destructured Sidebar.tsx

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,42 +1,15 @@
-"use client"
-
 import styles from "./Sidebar.module.css"
 
 import Image from "next/image"
 import Link from "next/link"
-
-import { useRouter } from "next/navigation"
-import { useAppDispatch, useAppSelector } from "@/store/store"
-import { signOut } from "@/store/features/userSlice"
+import SidebarPersonal from "@components/Sidebar/SidebarPersonal/SidebarPersonal"
 
 
 export default function Sidebar() {
-  const router   = useRouter()
-  const dispatch = useAppDispatch()
-  const user = useAppSelector((state) => state.user.user)
-
-  function handleSigning() {
-    if (user && user.username)
-      dispatch(signOut())
-    else
-      router.push("/sign/in")
-  }
 
   return (
     <div className={styles.sidebar}>
-      <div className={styles.sidebarPersonal}>
-        {
-          user && user.username
-            && (
-              <p className={styles.sidebarPersonalName}>{user.username}</p>
-            )
-        }
-        <div className={styles.sidebarIcon} onClick={handleSigning}>
-          <svg>
-            <use xlinkHref="/img/icon/sprite.svg#icon-logout" />
-          </svg>
-        </div>
-      </div>
+      <SidebarPersonal />
 
       <div className={styles.sidebarList}>
         <Link href="#">
---
+++ src/components/Sidebar/SidebarPersonal/SidebarPersonal.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import styles from "../Sidebar.module.css"
+
+import { useRouter } from "next/navigation"
+import { useAppDispatch, useAppSelector } from "@/store/store"
+import { signOut } from "@/store/features/userSlice"
+
+
+export default function SidebarPersonal() {
+  const router   = useRouter()
+  const dispatch = useAppDispatch()
+  const user     = useAppSelector((state) => state.user.user)
+
+  function handleSigning() {
+    if (user && user.username)
+      dispatch(signOut())
+    else
+      router.push("/sign/in")
+  }
+
+  return (
+    <div className={styles.sidebarPersonal}>
+      {
+        user && user.username
+          && (
+            <p className={styles.sidebarPersonalName}>{user.username}</p>
+          )
+      }
+      <div className={styles.sidebarIcon} onClick={handleSigning}>
+        <svg>
+          <use xlinkHref="/img/icon/sprite.svg#icon-logout" />
+        </svg>
+      </div>
+    </div>
+  )
+}
