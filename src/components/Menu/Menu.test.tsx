be87eb7e01729393b920be0ab02fbb1d5a9b7883
Anton Voytov <august-ra@ya.ru>
2024-09-17 01:31:33 +0300

    Updated second test for Menu.tsx

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -17,7 +17,7 @@ function renderMenu() {
   }
 
   const tokens: TokensPair = {
-    access: "1",
+    access:  "1",
     refresh: "1",
   }
 
@@ -33,22 +33,39 @@ function renderMenu() {
 }
 
 describe("Menu Component", () => {
-  it("renders correctly without user", () => {
-    renderWithProviders(<Menu setIsOpened={() => {}} />)
+  describe("renders correctly without user", () => {
+    it("Just two link there", () => {
+      renderWithProviders(<Menu setIsOpened={() => {}} />)
 
-    const links = screen.getAllByText("Войти")
-    expect(links.length).toBeGreaterThan(0)
+      const links = screen.getAllByRole("link")
+      expect(links.length).toEqual(2)
+    })
+    it("Link with 'sing in' there", () => {
+      renderWithProviders(<Menu setIsOpened={() => {}} />)
+
+      const links = screen.getAllByText("Войти")
+      expect(links.length).toEqual(1)
+    })
   })
-  it("renders correctly with user", () => {
-    renderMenu()
 
-    const links = screen.getAllByRole("link")
-    expect(links.length).toBeGreaterThan(2)
-  })
-  it("renders correctly with user", () => {
-    renderMenu()
+  describe("renders correctly with user", () => {
+    it("All three links there", () => {
+      renderMenu()
 
-    const links = screen.getAllByText("Любимые треки")
-    expect(links.length).toBeGreaterThan(0)
+      const links = screen.getAllByRole("link")
+      expect(links.length).toEqual(3)
+    })
+    it("Link with 'favorite tracks' there", () => {
+      renderMenu()
+
+      const links = screen.getAllByText("Любимые треки")
+      expect(links.length).toEqual(1)
+    })
+    it("Link with 'sign out' there", () => {
+      renderMenu()
+
+      const links = screen.getAllByText("Выйти")
+      expect(links.length).toEqual(1)
+    })
   })
 })
