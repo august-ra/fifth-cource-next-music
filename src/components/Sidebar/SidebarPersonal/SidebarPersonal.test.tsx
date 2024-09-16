0eb52d78a875a44732e100f6151a29c3cda53d6e
Anton Voytov <august-ra@ya.ru>
2024-09-17 01:50:59 +0300

    Added third test for SidebarPersonal.tsx

---
+++ src/components/Sidebar/SidebarPersonal/SidebarPersonal.test.tsx
@@ -0,0 +1,44 @@
+import SidebarPersonal from "./SidebarPersonal"
+
+import "@testing-library/jest-dom"
+import { screen } from "@testing-library/react"
+import { renderWithProviders } from "@/utils/tests"
+import { TokensPair, UserType } from "@/types/usersTypes"
+import { getEmptyError } from "@/types/errorsTypes"
+
+
+function renderSidebarPersonal(username: string) {
+  const user: UserType = {
+    username,
+    _id:             0,
+    first_name: "test",
+    last_name:  "test",
+    email:     "t@t.t",
+  }
+
+  const tokens: TokensPair = {
+    access:  "1",
+    refresh: "1",
+  }
+
+  renderWithProviders(<SidebarPersonal />, {
+    preloadedState: {
+      user: {
+        user,
+        tokens,
+        errorMsg: getEmptyError(),
+      },
+    },
+  })
+}
+
+describe("SidebarPersonal Component", () => {
+  it("renders correctly with user", () => {
+    const name = "John Connor"
+
+    renderSidebarPersonal(name)
+
+    const element = screen.getByRole("paragraph")
+    expect(element.textContent).toEqual(name)
+  })
+})
